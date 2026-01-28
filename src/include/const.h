47084d370e2c85d72547ce4bddd6caa6c7518b01
uncognic <uncognic@proton.me>
2026-01-27 23:53:45 -0500

    Update const.h

--- src/include/const.h
+++ src/include/const.h
@@ -1,19 +1,19 @@
 #pragma once
 
 const int greenButton = 2;
-const int redButton = 3;
-const int blueButton = 4;
-const int yellowButton = 5;
-const int whiteButton = 6;
-const int orangeButton = 7;
+const int redButton = 4;
+const int blueButton = 7;
+const int yellowButton = 14;
+const int whiteButton = 15;
+const int orangeButton = 16;
 
-const int green = 9;  
-const int red = 10;  
+const int green = 9;
+const int red = 10;
 const int blue = 11;
 const int yellow = 12;
 const int white = 13;
-const int orangeRed = 8;
-const int orangeGreen = 14;
-const int orangeBlue = 16;
+const int orangeRed = 3;
+const int orangeGreen = 5;
+const int orangeBlue = 6;
 
-const int buzzerPin = 15;
+const int buzzerPin = 17;
